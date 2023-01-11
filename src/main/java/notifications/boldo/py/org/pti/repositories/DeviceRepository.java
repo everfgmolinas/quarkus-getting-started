19e42d5f7abbbd6d245d204a9a36f3d76cb331e4
Ever Garay <everfgmolinas@hotmail.com>
2023-01-11 10:26:40 -0300

    fix: add applicationScoped anotation

--- src/main/java/notifications/boldo/py/org/pti/repositories/DeviceRepository.java
+++ src/main/java/notifications/boldo/py/org/pti/repositories/DeviceRepository.java
@@ -1,8 +1,11 @@
 package notifications.boldo.py.org.pti.repositories;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import notifications.boldo.py.org.pti.entities.Device;
 
+@ApplicationScoped
 public class DeviceRepository implements PanacheRepository<Device> {
 
     public Device update(Device device) {
